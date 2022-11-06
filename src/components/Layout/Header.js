bfe720c26bb64d3cdb9f6e40ae66323b129af7a1
Hemant Malokar <hemantmalokar@gmail.com>
2022-11-06 11:54:40 +0000

    Food Order App by Using React.

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import { Fragment } from "react";
+
+import HeaderCartButton from "./HeaderCartButton";
+import mealsImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={classes.header}>
+                <h2>Foodie</h2>
+                <HeaderCartButton onClick={props.onShowCart} />
+            </header>
+            <div className={classes["main-image"]}>
+                <img src={mealsImage} alt="A table full of Delicious Food" />
+            </div>
+        </Fragment>
+    );
+};
+
+export default Header;
